985569a31a2ba0290c04d07f3b30fe3d742e7451
snw <charles@stackadapt.com>
2019-10-26 22:31:35 -0400

    hugo

--- config/statsd.rb
+++
@@ -1,4 +0,0 @@
-require 'statsd-instrument'
-
-StatsD.backend = StatsD::Instrument::Backends::UDPBackend.new("statsd.hostedgraphite.com:8125", :statsd)
-StatsD.prefix = "ccdbadec-4d02-4f3e-98f8-5f41b99ff741"
